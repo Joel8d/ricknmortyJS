0dd8953cfe36c233caa78613a751de1b665a4f5e
Joel8d <joel8d@gmail.com>
2020-08-02 17:20:20 -0500

    Update Script xmlhttprequest

--- Scripts/challenge.js
+++ Scripts/challenge.js
@@ -1,31 +1,27 @@
-/*let XMlHTTPRequest = require('../node_modules/xmlhttprequest').XMLHttpRequest;*/
+/* let XMlHTTPRequest = require('../node_modules/xmlhttprequest').XMLHttpRequest; */
 
-
-//Definimos la api
 let API = "https://rickandmortyapi.com/api/character/"
 
-const fetchData = (url_api, callback) => {
+function fetchData(url_api, callback){
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", url_api, true);
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4) {
         if(xhttp.status === 200) {
-          callback(null, JSON.parse(xhttp.responseText));
+            callback(xhttp.responseText);
         } else {
-          callback(new Error("Error"), null);
+            callback(new Error("Error"), null);
         }
       }
     };
     xhttp.send();
   };
 
-//hacer petición a la API
-
 
 fetchData(API, (error1, data1) =>{
-    //crear y obtener la primera info
+
     if (error1) return console.log(error1)
-    //petición al primer personaje de la API
+
     fetchData(API + data1.results[0].id), (error2, data2) =>{
         if (error2) return console.error(error2)
         fetchData(data2.origin.url,  (error3, data3)=>{
