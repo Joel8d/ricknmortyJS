d5451872675ab8b5c5a5e462e6b63209d08f4c7a
Joel8d <joel8d@gmail.com>
2020-08-02 22:07:33 -0500

    Callback y tarjetas
    Callback y tarjetas funcionando con generador automatico

--- Scripts/challenge.js
+++ Scripts/challenge.js
@@ -1,36 +1,55 @@
 /* let XMlHTTPRequest = require('../node_modules/xmlhttprequest').XMLHttpRequest; */
 
-let API = "https://rickandmortyapi.com/api/character/"
+
+
+let API = "https://rickandmortyapi.com/api/character/",
+contenedor_callback = document.getElementById("contenedor_callback");
+const elementos_seccion = 20;
 
 function fetchData(url_api, callback){
     const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url_api, true);
+    xhttp.open('GET', url_api, true);
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4) {
         if(xhttp.status === 200) {
-            callback(xhttp.responseText);
+            callback(null, JSON.parse(xhttp.responseText));
         } else {
             callback(new Error("Error"), null);
         }
       }
-    };
-    xhttp.send();
-  };
-
-
-fetchData(API, (error1, data1) =>{
-
-    if (error1) return console.log(error1)
-
-    fetchData(API + data1.results[0].id), (error2, data2) =>{
-        if (error2) return console.error(error2)
-        fetchData(data2.origin.url,  (error3, data3)=>{
-            if (error3) return console.error(error3)
-            console.log(data1.info.count)
-            console.log(data2.name)
-            console.log(data3.dimension)
-        })
     }
+    xhttp.send();
+  }
 
-})
+
+
+    fetchData(API, (error1, data1) =>{            
+        if (error1) return console.log(error1)
+        data1.results.map(personajes => {
+
+        })
+        for(var i=0; i < elementos_seccion; i++){
+            const card = document.createElement("article");
+        card.innerHTML = 
+        `
+        <div class="contenedor_tarjeta">
+            <div class="contenedor_imagen">
+                <a>
+                    <img src="${data1.results[i].image}">
+                </a>
+            </div>
+            <div class="contenedor_descripcion">
+                <h2>${data1.results[i].name}</h2>
+                <p>${data1.results[i].species}</p>
+                <p>${data1.results[i].location.name}</p>
+            </div>
+        </div>
+        `
+
+        contenedor_callback.appendChild(card);
+
+        }
+
+        
+    })
 
